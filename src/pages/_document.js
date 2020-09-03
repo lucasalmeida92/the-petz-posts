c24d568d7f9d04d26824b84602faa6b6e71daf90
Lucas Almeida Siqueira <lucasalmeida70@gmail.com>
2020-09-03 05:05:16 -0300

    Changing document language to English

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -31,7 +31,7 @@ export default class MyDocument extends Document {
 
   render() {
     return (
-      <Html>
+      <Html lang="en">
         <Head>
           <link rel="icon" href="/favicon.ico" />
           <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;800&display=swap" rel="stylesheet"></link>
